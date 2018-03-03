0e7132153c735750faba4d7ab819adf519a8c327
Kartik Patel <kartik@Kartiks-MacBook-Pro.local>
2018-03-04 08:38:26 +1300

    changes

--- src/interests/Interests.jsx
+++ src/interests/Interests.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-
+import './interests.css';
 
 class Interests extends Component {
   render() {
     return (
-      <div className="Home">
+      <div className="interests">
           <h1>Interests and Hobbies</h1>
+
+          <p className="interests-info">My interests and hobbies vary from operating in the technology industry and operating within teams to being a customer representative and playing sports. I enjoy and have a passion for developing front-end web applications for clients, which suits their needs. This passion further extends my ability to self-learn and understand how new frameworks operate and the impact it can have on a clients web application. My skill set varies from tools to develop websites, progress tracking tools, collaborative code communication tools and modern frameworks. Furthermore, when I have been given the opportunity to lead a team and take charge, I always like to take the opportunity to lead a team to success. 
+			<br/><br/>
+			My hobbies involve me to get involved in social events within team activities to increase the relationship and communication ways within the teammates. My favorite hobby for me is to maintain a fit and healthy body by playing social or competitive sports and going to the gym. 
+			</p>
       </div>
     );
   }
