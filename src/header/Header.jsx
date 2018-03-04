0627a65282f08c16e32ad403a79101ff009efeb3
Kartik Patel <kartik@Kartiks-MacBook-Pro.local>
2018-03-04 18:17:30 +1300

    drop down added

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,15 +4,32 @@ import './header.css';
 export class Header extends Component{
     render(){
         return(
-            <div className="nav">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/skills">Skills</a></li>
-                    <li><a href="/experience">Work History</a></li>
-                </ul>
+            <div>
+                <div className="nav">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+
+                        <li className="dropdown">
+                            <a href="javascript:void(0)" className="dropbtn">Skills</a>
+                            <div className="dropdown-content">
+                              <a href="#">Personal Skills</a>
+                              <a href="#">Language Skills</a>
+                            </div>
+                        </li>
+
+                        <li><a href="/experience">Work History</a></li>
+                    </ul>
+                </div>
+
+
+
+
+
+               
             </div>
         );
     }
 }
 
 export default Header;
+
